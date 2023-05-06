ab31350af39db3ae5f597b5451190c70eb67e2fa
NAHID HASAN <129680386+nahidAK005@users.noreply.github.com>
2023-05-06 21:07:34 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,72 @@
+let menu = document.querySelector("menu-bars");
+let navbar = document.querySelector("navbar");
+
+
+  menu.onclick = () =>{
+    menu.classList.toggle("fa-time");
+    navbar.classList.toggle("active");
+
+
+
+
+
+  }
+
+
+
+
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+   loop: true,
+  
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const progressCircle = document.querySelector(".autoplay-progress svg");
+  // const progressContent = document.querySelector(".autoplay-progress span");
+  // var swiper = new swiper(".home-slider", {
+  //   spaceBetween: 30,
+  //   centeredSlides: true,
+  //   autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev"
+  //   },
+  //   on: {
+  //     autoplayTimeLeft(s, time, progress) {
+  //       progressCircle.style.setProperty("--progress", 1 - progress);
+  //       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+  //     }
+  //   }
+  // });
